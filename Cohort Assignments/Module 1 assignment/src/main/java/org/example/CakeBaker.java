e31a3d2cc628856d36475179ede1e3c572d918f8
Rajat Rathod <rajatrathod756@gmail.com>
2026-05-26 10:36:52 +0530

    Annotation fixed from @Controller to @Component and @Autowired removed from constructor injection

--- Cohort Assignments/Module 1 assignment/src/main/java/org/example/CakeBaker.java
+++ Cohort Assignments/Module 1 assignment/src/main/java/org/example/CakeBaker.java
@@ -2,15 +2,14 @@ package org.example;
 
 import org.example.impl.ChocolateFrosting;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-
-@Controller
+@Component
 public class CakeBaker {
     private final Frosting frosting;
     private final Syrup syrup;
 
-    @Autowired
     public CakeBaker(Frosting frosting, Syrup syrup) {
         this.frosting = frosting;
         this.syrup = syrup;
